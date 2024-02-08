074b7252bfefa6d82516fec706cb7f15d0c6a5a7
Maroussia Soa <soamaroussia@gmail.com>
2024-02-09 02:33:45 +0300

    public

--- src/main/java/cloud/voiture/controller/AnnoncePublicController.java
+++ src/main/java/cloud/voiture/controller/AnnoncePublicController.java
@@ -4,15 +4,20 @@ import cloud.voiture.service.AnnonceFavorisService;
 import cloud.voiture.service.AnnonceService;
 import cloud.voiture.model.Annonce;
 import cloud.voiture.model.AnnonceFavoris;
+import cloud.voiture.model.Carburant;
+import cloud.voiture.model.Categorie;
 import cloud.voiture.model.Marque;
 import cloud.voiture.model.ResponseWrap;
 import cloud.voiture.model.request.FiltreAnnonceReq;
+import cloud.voiture.repository.CarburantRepository;
+import cloud.voiture.repository.CategorieRepository;
 import cloud.voiture.repository.MarqueRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,10 +38,25 @@ public class AnnoncePublicController {
     private AnnonceService annonceService;
     @Autowired
     private MarqueRepository marqueRepository;
+    @Autowired
+    private CategorieRepository categorieRepository;
+
+    @Autowired
+    CarburantRepository carburantRepository;
+    
+    @GetMapping("carburant")
+    public ResponseEntity<ResponseWrap<List<Carburant>>> getAllCarburant() {
+        return new ResponseEntity<>(ResponseWrap.success(carburantRepository.findAll()),HttpStatus.OK);
+    }
 
     @GetMapping("marque")
     public ResponseWrap<List<Marque>> getAllMarque() {
         return ResponseWrap.success(marqueRepository.findByEtat(1));
+    }
+
+    @GetMapping("categorie")
+    public ResponseWrap<List<Categorie>> getAllCategorie() {
+        return ResponseWrap.success(categorieRepository.findByEtat(1));
     }
 
     // mahita annonce rehetra na tsy connecter aza
